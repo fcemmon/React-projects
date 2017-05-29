6e8ce56ce500ac1082ef52cba45877bdab253bc4
fcemmon <fcemmon@gmail.com>
2017-05-29 20:32:53 +0300

    sample project

---
+++ SoundWise/config.js
@@ -0,0 +1,12 @@
+  //  Firebase and stripe keys
+
+module.exports.config = {
+    apiKey: "AIzaSyAt1hJ8lUnK0S9LgP2rp5c7NfwM0zLI9Rk",
+    authDomain: "soundwise-a8e6f.firebaseapp.com",
+    databaseURL: "https://soundwise-a8e6f.firebaseio.com",
+    storageBucket: "soundwise-a8e6f.appspot.com",
+    messagingSenderId: "833450953424"
+}
+
+module.exports.stripe_key = 'sk_live_tZyWGzYgQfmR7UeY4NV4yx7A'
+// this is the live key, not for testing.
